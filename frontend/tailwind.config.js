0d00d6750e544d84b39eb20595ac86566f76ee84
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-01 00:45:06 -0400

    Colors config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0067ff',
+        yellowColor: '#feb60d',
+        purpleColor: '#9771ff',
+        irisBlueColor: '#01b5c5',
+        headingColor: '#1b1a1e',
+        textColor: '#4e545f',
+      },
+      boxShadow: {
+        panelShadow: 'rgba(17, 12, 46, 0.15) 0px 48px 100px 0px',
+      },
+    },
   },
   plugins: [],
 }
-

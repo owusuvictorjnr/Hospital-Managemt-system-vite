9fb19380050e51405c93a6ffdd53ed0b92eea999
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-02 22:07:51 -0400

    routes for the pages

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+// import React from 'react'
+import Home from '../pages/Home'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetails'
+import Signin from '../pages/Signin'
+import Signup from '../pages/Signup'
+import Contact from '../pages/Contact'
+import Services from '../pages/Services'
+
+import { Routes, Route } from 'react-router-dom'
 
 const Routers = () => {
   return (
-    <div>Routers</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
   )
 }
 
